593fbb72ad32b4f8184c13b02cf812612f5caf95
ankursharmanitj <ankursharmanitj@gmail.com>
2017-02-16 12:36:20 +0530

    added support for safari icon

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,17 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/ie-icon.png');
-    } else if(mySrc === 'images/ie-icon.png') {
-	  myImage.setAttribute ('src','images/chrome-icon.png');
-	} else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
-    }
+	    myImage.setAttribute ('src','images/chrome-icon.png');
+    }	
+	else if(mySrc === 'images/chrome-icon.png') {		
+		myImage.setAttribute ('src','images/safari-icon.png');
+	}
+	else if(mySrc === 'images/safari-icon.png') {		
+		myImage.setAttribute ('src','images/ie-icon.png');
+	}
+	else {
+		myImage.setAttribute ('src','images/firefox-icon.png');
+	}
 }
 
 var myButton = document.querySelector('button');
